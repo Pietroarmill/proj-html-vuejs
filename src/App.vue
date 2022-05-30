333ffcaa970859c2d9f006b23126337a1e849788
Pietroarmill <pietroarmillotta38@gmail.com>
2022-05-30 17:22:46 +0200

    add blogsSection

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <AppHeader :ElementTop="this.headerTopArray" :ElementBottom="this.headerBottomArray"/>
-    <AppMain />
+    <AppHeader :ElementTop="this.headerTopArray" :ElementBottom="this.headerBottomArray" />
+    <AppMain :blogsArray="this.blogs"/>
     <AppFooter />
   </div>
 </template>
@@ -49,6 +49,44 @@ export default {
         {
           name: "shop",
           active: false
+        },
+      ],
+      blogs: [
+        {
+          title: "How To Take Better Concert Picture in 30 Seconds",
+          content:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio blanditiis dolorem ex reprehenderit sed maiores cum",
+          type: "photography",
+          src: "-46",
+        },
+        {
+          title: "Gadgets That Make Yout Smartphone Even Smarter",
+          content:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio blanditiis dolorem ex reprehenderit sed maiores cum",
+          type: "gadgets",
+          src: "-47",
+        },
+        {
+          title: "20 Top-Rated Tourist Attractions in Manhattan",
+          type: "travel",
+          content:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio blanditiis dolorem ex reprehenderit sed maiores cum",
+          src: "-48",
+        },
+        {
+          title: "The Best Way To Ride a Motorcylce",
+          type: "lifestyle",
+          content:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio blanditiis dolorem ex reprehenderit sed maiores cum",
+          src: "-49",
+        },
+        {
+          title: "5 Fun Things to Do at the Beach",
+          type: "travel",
+          content:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio blanditiis dolorem ex reprehenderit sed maiores cum",
+          src: "-50",
+        },
+        {
+          title: "Amazingly Fresh Fruit And Herb Drinks For Summer",
+          type: "recipes",
+          content:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio blanditiis dolorem ex reprehenderit sed maiores cum",
+          src: "-51",
         },
       ]
     }
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,16 +1,101 @@
 <template>
-  <div>
-    SONO IL MAIN
-  </div>
+  <!-- MAIN -->
+  <main class="main">
+    <div class="container">
+
+      <!-- Main Blogs Section -->
+      <section class="main-blogs-section">
+        <div class="blog-banner with-overlay" v-for="(item, index) in blogsArray" :key="index">
+
+          <div class="blog-img">
+            <img :src="require(`../assets/img/blog${item.src}.jpg`)" alt="">
+          </div>
+
+          <!-- text blog -->
+          <div class="text-blog">
+            <span class="blog-type">{{ item.type.toUpperCase() }}</span>
+            <h2 class="blog-title">{{ item.title }}</h2>
+            <p class="blog-content">{{ item.content }}</p>
+          </div>
+          <!-- /text blog -->
+        </div>
+
+      </section>
+      <!-- /Main Blogs Section -->
+
+      <!-- Main Post Section -->
+      <section class="main-post-section">
+      </section>
+      <!-- /Main Post Section -->
+
+    </div>
+  </main>
+  <!-- /MAIN -->
 </template>
 
 <script>
+
 export default {
   name: "AppMain",
+  props: {
+    blogsArray: Array,
+  },
+  components: {
 
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+@import "../style/variables.scss";
 
+.main {
+  background-color: #F7F7F7;
+  width: 100%;
+  padding: 2rem 0;
+
+  &-blogs-section {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+
+    .blog-banner {
+      width: calc(96% / 3);
+      display: flex;
+      align-items: flex-end;
+      color: white;
+      margin: 1rem 0;
+      position: relative;
+      overflow: hidden;
+
+      .text-blog {
+        position: absolute;
+        margin: 0 0 1rem 1rem;
+
+        .blog-type {
+          padding: .3rem .6rem;
+          background-color: $brand-blog-color;
+          font-size: .5rem;
+        }
+
+        .blog-title {
+          font-size: 1.3rem;
+          margin-top: .7rem;
+        }
+
+        .blog-content {
+          display: none;
+          color: #B7B7B7;
+          font-weight: bold;
+          line-height: 25px;
+        }
+      }
+
+      &:hover .blog-content {
+        display: block;
+      }
+    }
+  }
+}
 </style>
